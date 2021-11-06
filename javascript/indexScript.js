487b53da301b4420dd6d166c9bc5b0d98555edd6
Eldar Aharonson <eaharonson1806@gmail.com>
2021-11-06 20:36:46 +0200

    trial of json integration

--- javascript/indexScript.js
+++ javascript/indexScript.js
@@ -1,6 +1,18 @@
 
-fetch("/json/pageData.json")
-        .then((response) => response.json())
-        .then((data) => {
-          document.getElementById("intro-text").innerHTML = data.introText;
-        });
+
+
+// fetch("../json/pageData.json")
+//         .then((response) => response.json())
+//         .then((data) => {
+//           document.getElementById("intro-text").innerHTML = data.introText;
+//         });
+
+// var data = JSON.parse(introText);
+// document.getElementById("intro-text").innerHTML = data;
+
+var request = new XMLHttpRequest();
+request.open("GET","https://eldaraharonson.github.io/Bootstrap-Portfolio/json/pageData.json", false);
+request.send(null);
+var my_JSON_object = JSON.parse(request.responseText);
+document.getElementById("intro-text").innerHTML = my_JSON_object.introText;
+  
