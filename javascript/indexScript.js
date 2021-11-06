e441a926c1d7b2000f39cd6b69e5315ea148e586
Eldar Aharonson <eaharonson1806@gmail.com>
2021-11-06 21:09:28 +0200

    json integration seems fixed

--- javascript/indexScript.js
+++ javascript/indexScript.js
@@ -1,15 +1,4 @@
 
-
-
-// fetch("../json/pageData.json")
-//         .then((response) => response.json())
-//         .then((data) => {
-//           document.getElementById("intro-text").innerHTML = data.introText;
-//         });
-
-// var data = JSON.parse(introText);
-// document.getElementById("intro-text").innerHTML = data;
-
 var request = new XMLHttpRequest();
 request.open("GET","https://eldaraharonson.github.io/Bootstrap-Portfolio/json/pageData.json", false);
 request.send(null);
--- javascript/portfolioScript.js
+++ javascript/portfolioScript.js
@@ -1,5 +1,5 @@
 
-// fetch("../json/pageData.json")
+
 fetch("https://eldaraharonson.github.io/Bootstrap-Portfolio/json/pageData.json")
         .then((response) => response.json())
         .then((data) => {
